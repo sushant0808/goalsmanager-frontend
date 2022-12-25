14aefa7823fdae29a897c6970705cea548004118
Sushant <sushantmore09072000@gmail.com>
2022-12-25 18:17:37 +0530

    misc

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://goalsmanager-api.onrender.com/',
+    // baseURL: 'https://goalsmanager-api.onrender.com/',
+    baseURL: 'http://localhost:5000/',
 });
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import { store } from './Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log('Store hai ye',store);
 root.render(
   // <React.StrictMode>
 
