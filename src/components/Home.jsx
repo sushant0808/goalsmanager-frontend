3f63dea05f749c228a12ec585a07bcbd06cbc2a9
Sushant <sushantmore09072000@gmail.com>
2022-12-25 18:28:57 +0530

    misc

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,11 @@ import { Link, useNavigate } from 'react-router-dom'
 import Cookies from 'js-cookie'
 
 const Home = () => {
+    console.log('Home is called')
     const navigate = useNavigate();
     useEffect(() => {
         const token = Cookies.get('token');
+        console.log('token in home',token);
         if(!token){
             navigate('/login');
         }else{
